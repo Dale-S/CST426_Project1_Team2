425d0c82bd84294b0d53dd28c3d8350f0cb75085
MGarc758 <83996652+MGarc758@users.noreply.github.com>
2023-09-11 00:50:08 -0700

    Added Basic Main Menu

---
+++ Assets/scripts/MainMenuController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void StartGame()
+    {
+        // Load the "Game" scene
+        SceneManager.LoadScene("");
+    }
+
+    public void QuitGame()
+    {
+        // Quit the application (only works in a build, not in the editor)
+        Application.Quit();
+    }
+}
