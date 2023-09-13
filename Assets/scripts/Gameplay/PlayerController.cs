01186d421ef0a6eabf60d0b71f5162fc4d7847c0
ConlynPattison <cpattison@csumb.edu>
2023-09-12 22:18:32 -0700

    Lerped rotation added to replace instant turn

--- Assets/scripts/Gameplay/PlayerController.cs
+++ Assets/scripts/Gameplay/PlayerController.cs
@@ -7,30 +7,38 @@ public class PlayerController : MonoBehaviour
     [Header("Face Controls")] [Tooltip("Degrees rotated in +/- Y direction per turn")]
     public int spinDegrees = 90;
 
+    [Tooltip("Speed of turning")] public float spinSpeed = 15.0f;
+
     private Transform _initTransform;
+    private Quaternion _targetRotation;
+    private int _direction;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         _initTransform = gameObject.transform;
+        _targetRotation = _initTransform.rotation;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
+    {
+        UpdateRotation();
+    }
+
+    private void UpdateRotation()
     {
         if (Input.GetKeyDown(KeyCode.A)) /* Left Turn */
         {
-            TurnPlayer(-1);
+            _direction = -1;
+            _targetRotation *= Quaternion.AngleAxis(spinDegrees * _direction, Vector3.up);
         }
-
-        if (Input.GetKeyDown(KeyCode.D)) /* Right Turn */
+        else if (Input.GetKeyDown(KeyCode.D)) /* Right Turn */
         {
-            TurnPlayer(1);
+            _direction = 1;
+            _targetRotation *= Quaternion.AngleAxis(spinDegrees * _direction, Vector3.up);
         }
-    }
 
-    void TurnPlayer(int direction)
-    {
-        gameObject.transform.Rotate(Vector3.up, direction * spinDegrees);
+        transform.rotation = Quaternion.Lerp(transform.rotation, _targetRotation, spinSpeed * Time.deltaTime);
     }
 }
